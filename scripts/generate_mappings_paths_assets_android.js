1cf90dd29e48251ad55f7224f5d95c13b68cd74f
Wave Dev <66224387+wavedevgit@users.noreply.github.com>
2026-06-04 13:12:17 +0100

    add actions support (might explode)

--- scripts/generate_mappings_paths_assets_android.js
+++ scripts/generate_mappings_paths_assets_android.js
@@ -23,6 +23,7 @@ await fs.writeFile(
 
 const result = [];
 const experiments = [];
+const actions = [];
 
 for (const item of data) {
     // only discord_assets have extractable matcher
@@ -59,6 +60,47 @@ for (const item of data) {
         };
         experiments.push(res);
     }
+    // actions, uses endpoints used and methods
+    if (item.file.endsWith('ActivitiesActionCreators.tsx')) {
+        const content = (
+            await get(BASE_URL.concat('/source/', item.file))
+        ).split('\n');
+        let res = { path: item.file, flux_events: [] };
+        let event = { type: null, var: null };
+        let isStartOfDispatch = false;
+        for (let i = 0; i < content.length; i++) {
+            const line = content[i];
+            if (line.includes('.dispatch;')) {
+                isStartOfDispatch = true;
+                continue;
+            }
+            const string = line.match(
+                /(?<var>var\d+)\s*=\s*["'](?<value>[A-Z_]+)["']/,
+            )?.groups;
+            if (isStartOfDispatch && string) {
+                event.var = string.var;
+                event.type = string.value;
+                continue;
+            }
+            const typeSetTo = line.match(
+                /var\d+\[['|"]type['|"]\]\s*=\s*(var\d+)/,
+            )?.[1];
+
+            if (!!typeSetTo && typeSetTo === event.var) {
+                isStartOfDispatch = false;
+                res.flux_events.push(event.type);
+                event = { type: null, var: null };
+            }
+        }
+
+        if (res.flux_events.length !== 0)
+            actions.push({
+                path: res.path,
+                flux_events: res.flux_events
+                    .sort((a, b) => b.length - a.length)
+                    .slice(0, 5),
+            });
+    }
 }
 
 const { rawManifest: manifest } = JSON.parse(await get(ANDROID_MANIFST_URL));
@@ -79,6 +121,11 @@ if (process.argv?.[2] !== 'only-experiments')
 
 await fs.writeFile(
     './data/files/experiments_android.json',
+    JSON.stringify(experiments, null, 4),
+    'utf-8',
+);
+await fs.writeFile(
+    './data/files/actions_android.json',
     JSON.stringify(experiments, null, 4),
     'utf-8',
 );
