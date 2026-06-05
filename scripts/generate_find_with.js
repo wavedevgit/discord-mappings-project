426e92af3bb7ff6175f00de32fb684ab8316513d
Wave Dev <66224387+wavedevgit@users.noreply.github.com>
2026-06-05 17:53:11 +0100

    another try

--- scripts/generate_find_with.js
+++ scripts/generate_find_with.js
@@ -87,10 +87,7 @@ const actions = actionsandroid.map((e) => ({
         : e.functions_names.map((name) => ({
               type: 'regexp',
               value: new RegExp(
-                  `(?:async\\s+)?function\\s+${name}\\s*\\([^)]*\\)\\s*\\{|` +
-                      `${name}\\s*:\\s*(?:async\\s*)?\\([^)]*\\)\\s*=>|` +
-                      `${name}\\s*\\([^)]*\\)\\s*\\{|` +
-                      `${name}\\s*:\\s*(?:async\\s+)?function\\s*\\([^)]*\\)\\s*\\{`,
+                  `(?:async\\s+)?(?:function\\s+${name}\\s*\\([^)]*\\)\\s*\\{|${name}\\s*:\\s*(?:async\\s+)?\\([^)]*\\)\\s*=>)`,
               ).source,
               flags: '',
           })),
