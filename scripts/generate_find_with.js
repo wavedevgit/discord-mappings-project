be9bea1a773eccbcea573d1366ea29e218977f8f
Wave Dev <66224387+wavedevgit@users.noreply.github.com>
2026-06-05 17:07:53 +0100

    improve regex to add support for most stuff

--- scripts/generate_find_with.js
+++ scripts/generate_find_with.js
@@ -87,7 +87,7 @@ const actions = actionsandroid.map((e) => ({
         : e.functions_names.map((name) => ({
               type: 'regexp',
               value: new RegExp(
-                  `(?:async\\s+)?function\\s+${name}\\s*\\([^)]*\\)\\s*\\{`,
+                  `(?:async\\s+)?(?:function\\s+${name}\\s*\\([^)]*\\)\\s*\\{|${name}\\s*:\\s*(?:async\\s+)?\\([^)]*\\)\\s*=>)`,
               ).source,
               flags: '',
           })),
